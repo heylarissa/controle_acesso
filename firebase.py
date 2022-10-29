e59cceb7226ba364a8a34752c8ab867eb4971b89
heylarissa <larissah.dandrade@gmail.com>
2022-10-29 18:32:28 -0300

    segmentando código

--- firebase.py
+++ firebase.py
@@ -1,24 +1,40 @@
 #importar o pyrebase4 no pip
 import pyrebase
+import os
+import dotenv
+from pathlib import Path
+
+def create_user(auth):
+    user = input("Digite seu e-mail: ")
+    password = input("Digite sua senha, com pelo menos 6 caracteres: ")
+    status = auth.create_user_with_email_and_password(user,password)
+    print("Resultado: ", status)
+
+
+dotenv.load_dotenv(
+    dotenv_path=os.path.join(Path(__file__).parent, '.env')
+)
 
 firebaseConfig = {
-    "apiKey": "",
-    "authDomain": "",
-    "projectId": "",
-    "databaseURL": "https://" + "<ID_APP>" + ".firebaseio.com",
-    "storageBucket": "",
-    "messagingSenderId": "",
-    "appId": "",
-    "measurementId": ""
+    "apiKey": os.environ.get("APIKEY"),
+    "authDomain": os.environ.get("APIKEY"),
+    "projectId": os.environ.get("APIKEY"),
+    "databaseURL": os.environ.get("APIKEY"),
+    "storageBucket": os.environ.get("APIKEY"),
+    "messagingSenderId": os.environ.get("APIKEY"),
+    "appId": os.environ.get("APIKEY"),
+    "measurementId": os.environ.get("APIKEY"),
 }
 
-firebase = pyrebase.initialize_app(firebaseConfig)
-auth = firebase.auth()
 
-user = input("Digite seu e-mail: ")
-password = input("Digite sua senha, com pelo menos 6 caracteres: ")
-status = auth.create_user_with_email_and_password(user,password)
-print("Resultado: ", status)
+def main():
+    firebase = pyrebase.initialize_app(firebaseConfig)
+    auth = firebase.auth()
 
+    create_user(auth)
+
+
+if __name__=="__main__":
+    main()
 
 
